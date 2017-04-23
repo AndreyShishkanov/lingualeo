d91b2b6c107781472b25f9c5d2683624e10219f2
AndreyShishkanov <goodzon91@gmail.com>
2017-04-23 14:40:21 +0300

    first commit

---
+++ LinguaLeo/LinguoApi.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace LinguaLeo
+{
+    public class LinguaLeoApi
+    {
+        private static CookieWebClient WebClient { get; set; }
+
+        public Translate[] Translate { get; set; }
+        public bool is_new { get; set; }
+
+        //public static Translate GetTranslation(string word)
+        //{
+        //    using (var webclient = new CookieWebClient())
+        //    {
+        //        webclient.QueryString.Add("word", word);
+        //        webclient.Encoding = Encoding.UTF8;
+        //        var response = webclient.DownloadString(@"http://api.lingualeo.com/gettranslates");
+        //        var json = JsonConvert.DeserializeObject<LinguaLeoApi>(response);
+        //        return json.Translate.FirstOrDefault();
+        //    }
+        //}
+
+        public static async Task<bool> AddWord(string word, string translation, string email, string password)
+        {
+            WebClient = new CookieWebClient();
+            await Auth(email, password);
+            WebClient.QueryString.Add("word", word.Trim());
+            if(!string.IsNullOrWhiteSpace(translation)) WebClient.QueryString.Add("tword", translation);
+            WebClient.Encoding = Encoding.UTF8;
+            
+            var response = await WebClient.DownloadStringTaskAsync(@"http://api.lingualeo.com/addword");
+            var json = JsonConvert.DeserializeObject<LinguaLeoApi>(response);
+
+            WebClient.Dispose();
+
+            return json.is_new;
+        }
+
+        public static async Task Auth(string email, string password)
+        {
+            WebClient.QueryString.Add("email", email);
+            WebClient.QueryString.Add("password", password);
+            await WebClient.DownloadStringTaskAsync(@"http://api.lingualeo.com/api/login");
+        }
+
+    }
+
+    public class Translate
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        public int Votes { get; set; }
+    }
+
+    public class CookieWebClient : WebClient
+    {
+        public CookieContainer CookieContainer { get; private set; }
+
+        /// <summary>
+        /// This will instanciate an internal CookieContainer.
+        /// </summary>
+        public CookieWebClient()
+        {
+            CookieContainer = new CookieContainer();
+        }
+
+        /// <summary>
+        /// Use this if you want to control the CookieContainer outside this class.
+        /// </summary>
+        public CookieWebClient(CookieContainer cookieContainer)
+        {
+            CookieContainer = cookieContainer;
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var request = base.GetWebRequest(address) as HttpWebRequest;
+            if (request == null) return base.GetWebRequest(address);
+            request.CookieContainer = CookieContainer;
+            return request;
+        }
+    }
+}
---
+++ LinguaLeo/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Microsoft.Win32;
+
+namespace LinguaLeo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            var users = new Dictionary<string, string>
+            {
+                {"user1", "password1"},
+                {"user2", "password2"}
+            };
+
+            var fileDialog = new OpenFileDialog();
+            var result = fileDialog.ShowDialog()??false;
+            if (result)
+            {
+                var file = fileDialog.FileName;
+                DataTable tbl = new DataTable();
+                using (var pck = new OfficeOpenXml.ExcelPackage())
+                {
+                    using (var stream = System.IO.File.OpenRead(file))
+                    {
+                        pck.Load(stream);
+                    }
+                    var ws = pck.Workbook.Worksheets.First();
+
+                    tbl.Columns.Add(ws.Cells[1, 1, 1, 1].Text);
+                    tbl.Columns.Add(ws.Cells[1, 3, 1, 3].Text);
+
+                    var startRow = 2;
+                    for (int rowNum = startRow; rowNum <= ws.Dimension.End.Row; rowNum++)
+                    {
+                        DataRow row = tbl.Rows.Add();
+                        row[0] = ws.Cells[rowNum, 1, rowNum, 1].Text;
+                        row[1] = ws.Cells[rowNum, 3, rowNum, 3].Text;
+                    }
+                }
+
+                ProgressBar1.Maximum = tbl.Rows.Count;
+
+                foreach (var user in users)
+                {
+                    double value = 0;
+                    ProgressBar1.Value = value;
+
+                    foreach (DataRow row in tbl.Rows)
+                    {
+                        if (!await LinguaLeoApi.AddWord(row.ItemArray[0].ToString(), row.ItemArray[1].ToString(),user.Key,user.Value))
+                            textBox.Text += row.ItemArray[0] + "\r\n";
+                        value++;
+                        ProgressBar1.Dispatcher.Invoke(() => ProgressBar1.Value = value, DispatcherPriority.Background);
+                    }
+                }
+            }
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+
+
+}
